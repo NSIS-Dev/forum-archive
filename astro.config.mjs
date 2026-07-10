264c95c4f96d83f82fd52fec6d60f25ec7b751d1
Jan T. Sott <git@idleberg.com>
2026-07-10 19:12:05 +0200

    chore: update from template

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,21 +2,20 @@
 import { defineConfig } from 'astro/config';
 import icons from 'unplugin-icons/vite';
 import tailwindcss from '@tailwindcss/vite';
-import { githubPages, siteConfig } from './.castro/config.ts';
+import { dataSymlinks, githubPages, siteConfig } from './.castro/config.ts';
 
 export default defineConfig({
 	...githubPages(),
 	...siteConfig({
-    title: 'NSIS Discussion Archive',
-	  description: 'Mirror of the NSIS discussion forum on Winamp.com',
-    keywords: ['Nullsoft Scriptable Install System', 'nsis', 'windows installer', 'DSL', 'install script', 'windows setup'],
-  }),
+		title: 'Forum Archive',
+	}),
 	vite: {
 		plugins: [
 			icons({
 				compiler: 'astro',
 			}),
 			tailwindcss(),
+			dataSymlinks(),
 		],
 	},
 });
