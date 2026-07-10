151e10450b715cbb6dffb2146681c0e16ef566f7
Jan T. Sott <git@idleberg.com>
2026-07-10 14:42:01 +0200

    chore: update siteConfig

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,9 @@ import { githubPages, siteConfig } from './.castro/config.ts';
 export default defineConfig({
 	...githubPages(),
 	...siteConfig({
-    title: 'NSIS Discussion Archive'
+    title: 'NSIS Discussion Archive',
+	  description: 'Mirror of the NSIS discussion forum on Winamp.com',
+    keywords: ['Nullsoft Scriptable Install System', 'nsis', 'windows installer', 'DSL', 'install script', 'windows setup'],
   }),
 	vite: {
 		plugins: [
