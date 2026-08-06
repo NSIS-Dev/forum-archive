a038c698e6ae0af14e62bb8361a87877a44fbf92
Jan T. Sott <git@idleberg.com>
2026-08-07 00:07:32 +0200

    style: change default font

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,21 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import castro from './.castro/integration.ts';
 
 export default defineConfig({
 	integrations: [
 		castro({
-			title: 'NSIS Forum Archive',
-	  description: 'Mirror of the NSIS discussion forum on Winamp.com',
-    keywords: ['Nullsoft Scriptable Install System', 'nsis', 'windows installer', 'DSL', 'install script', 'windows setup'],
+			title: 'Forum Archive',
 			githubPages: true,
 		}),
 		sitemap(),
+	],
+	fonts: [
+		{
+			provider: fontProviders.fontsource(),
+			name: 'Libre Franklin',
+			cssVariable: '--font-sans',
+		},
 	],
 });
